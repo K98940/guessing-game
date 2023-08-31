dbb770beb216f039609129a02f3d85591189db59
k98940 <pv1xxb@yandex.ru>
2023-08-31 20:34:30 +0600

    feat: game done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
+        this.n = 0
     }
 
     guess() {
-
+        this.n = Math.ceil((this.min + this.max) / 2)
+        console.log(`guess=${this.n}, min=${this.min}, max=${this.max}`)
+        return this.n
     }
 
     lower() {
-
+        this.max = this.n
     }
 
     greater() {
-
+        this.min = this.n
     }
 }
 
